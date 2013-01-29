76b4b07c5be0ffd1e2ddf86d6f547ff0485cf343
Hui Peng Hu <woohp135@gmail.com>
2013-01-28 22:16:55 -0800

    generated project and added sorcery for authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuusicBase::Application.config.secret_token = '9dcaabf87b93422a49055caced35e7d76938d1660f1347745e585d1f823e5242d19aa46aabcf3c9f7ecfb2ca5377ed71bbb26acaf6ee83811bc21cef33c225ca'
